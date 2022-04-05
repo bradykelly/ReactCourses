903119c7245cd57f42c97d3c266aa52fbc0c848e
Brady Kelly <brady.kelly@xisystems.co.za>
2022-04-05 12:45:07 +0200

    New render with provider

--- React.ModernProjects/src/index.js
+++ React.ModernProjects/src/index.js
@@ -1,5 +1,12 @@
 ﻿import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import {Provider} from 'react-redux';
+import {configureStore} from "./store";
 import App from './App.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={configureStore()}>
+    <App />  
+  </Provider>
+);
