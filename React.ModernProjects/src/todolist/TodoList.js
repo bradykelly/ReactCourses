7036542c004440e2fe51f3eef0c6ad58ef6d4024
Brady Kelly <brady.kelly@xisystems.co.za>
2022-04-05 20:35:46 +0200

    Connected TodoList

--- React.ModernProjects/src/todolist/TodoList.js
+++ React.ModernProjects/src/todolist/TodoList.js
@@ -1,13 +1,23 @@
 ﻿import React from 'react';
+import { connect } from "react-redux";
+import { removeTodo } from "./actions";
 import TodoListItem from './TodoListItem';
 import NewTodoForm from "./NewTodoForm";
 import "./TodoList.css";
 
-const TodoList = ({todos = [{text: "Hello"}]}) => (
+const TodoList = ({ todos = [], onRemoveClicked }) => (
     <div className="list-wrapper">
-        <NewTodoForm/>
-        {todos.map(todo => <TodoListItem todo={todo}/>)}
+        <NewTodoForm />
+        { todos.map(todo => <TodoListItem todo={ todo } onRemoveClicked={ onRemoveClicked } />) }
     </div>
 );
 
-export default TodoList;
+const mapStateToProps = state => ({
+    todos: state.todos
+});
+
+const mapDispatchToProps = dispatch => ({
+    onRemoveClicked: text => dispatch(removeTodo(text))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
